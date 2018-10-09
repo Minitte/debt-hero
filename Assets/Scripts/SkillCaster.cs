d0096f55159e9bccf2e250d2d2f2ea0580dfc37c
MunifiSense <20971047+MunifiSense@users.noreply.github.com>
2018-10-08 21:32:56 -0700

    Reorganized skill system code

--- Assets/Scripts/SkillCaster.cs
+++ Assets/Scripts/SkillCaster.cs
@@ -109,82 +109,13 @@ public class SkillCaster : MonoBehaviour {
         }
         if(cast)
         {
-
+            if (Skill.GetInfo(skillID).type != -1)
+            {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("skill"+skillNum, true);
+            }
         }
     }
 
-    /// <summary>
-    /// Skill Type 1
-    /// To use a buff skill that buffs a stat by amount for duration
-    /// (i.e.) Gain("hp", 10); gain 10 hp
-    /// Should only be used for HP and MP
-    /// </summary>
-    public void Gain(string stat, float amount)
-    {
 
-    }
-    /// <summary>
-    /// Skill Type 2
-    /// To use a buff skill that buffs a stat by amount for duration
-    /// (i.e.) BuffDuration("hp", 10, 10); buffs MaxHP by 10 for 10 seconds
-    /// </summary>
-    public void BuffDuration(string stat, float duration, float amount)
-    {
-
-    }
-    /// <summary>
-    /// Skill Type 3
-    /// To use a buff skill that gives stat by amount/second for duration
-    /// (i.e.) BuffOverTime("hp", 10, 10); gives 10 HP per second for 10 seconds
-    /// </summary>
-    public void BuffOverTime(string stat, float duration, float amount)
-    {
-
-    }
-
-    /// <summary>
-    /// Skill Type 4
-    /// To use a melee skill that does damage
-    /// </summary>
-    public void Melee(float damage)
-    {
-
-    }
-
-    /// <summary>
-    /// Skill Type 5
-    /// To use a ranged skill
-    /// </summary>
-    public void Ranged(float damage)
-    {
-
-    }
-
-    /// <summary>
-    /// Gets info for a skill by Skill ID
-    /// </summary>
-    public Skill GetSkillInfo(int skillID)
-    {
-        Skill skill = new Skill();
-
-        switch (skillID)
-        {
-            case 0:
-                skill.name = "Power Strike";
-                skill.amount = 20;
-                skill.type = 4;
-                skill.coolDown = 20;
-                break;
-            case 1:
-                skill.name = "HP Buff";
-                skill.amount = 20;
-                skill.type = 2;
-                skill.coolDown = 30;
-                skill.duration = 0;
-                break;
-        }
-
-        return skill;
-    }
 
 }
