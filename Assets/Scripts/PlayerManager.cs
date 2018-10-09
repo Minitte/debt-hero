3624906741e701f3d6a39c5f4c3ea9c63096dac7
Minitte <arcentalminitte@gmail.com>
2018-10-08 18:33:10 -0700

    basic player manager

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	/// <summary>
+	/// Player prefab
+	/// </summary>
+	public GameObject playerPrefab;
+
+	/// <summary>
+	/// The current local player gameobject instance
+	/// </summary>
+	public GameObject localPlayer;
+
+	/// <summary>
+	/// Player's stats
+	/// </summary>
+	private CharacterStats _stats;
+
+	/// <summary>
+	/// Player's class
+	/// </summary>
+	private BaseClass _class;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake() {
+		//localPlayer = Instantiate(playerPrefab);
+
+		_stats = GetComponent<CharacterStats>();
+
+		_class = GetComponent<BaseClass>();
+	}
+}
