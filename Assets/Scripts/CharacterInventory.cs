69fd98d46da194b9d2491e2ed145ba825d787fd3
Davis Pham <arcentalminitte@gmail.com>
2018-10-13 11:26:23 -0700

    inventory gold

---
+++ Assets/Scripts/CharacterInventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInventory : MonoBehaviour {
+
+	/// <summary>
+	/// Amount of gold this character has
+	/// </summary>
+	public int gold;
+}
