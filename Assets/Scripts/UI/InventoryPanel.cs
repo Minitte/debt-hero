dfb32debcd5a1df330d43f03f4f2fe36b9273fa4
Davis Pham <arcentalminitte@gmail.com>
2018-10-13 19:59:59 -0700

    inventory panel gold display

---
+++ Assets/Scripts/UI/InventoryPanel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class InventoryPanel : MonoBehaviour {
+
+	/// <summary>
+	/// Text showing the gold amount
+	/// </summary>
+	public TextMeshProUGUI goldText;
+
+	/// <summary>
+	/// target inventory to show
+	/// </summary>
+	public CharacterInventory inventory;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake() {
+		UpdateInventoryDisplay();
+	}
+
+	/// <summary>
+	/// This function is called when the object becomes enabled and active.
+	/// </summary>
+	void OnEnable() {
+		UpdateInventoryDisplay();
+	}
+
+	/// <summary>
+	/// Updatse the inventory panel
+	/// </summary>
+	public void UpdateInventoryDisplay() {
+		goldText.text = inventory.gold + "g";
+	}
+}
