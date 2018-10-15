9cf7f63380ea26e9a8d4483bf57a4f557ba8e40c
MunifiSense <20971047+MunifiSense@users.noreply.github.com>
2018-10-14 23:59:14 -0700

    Added particles, level up particle, dungeon textures, Level up code

---
+++ Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour
+{
+    public int currentLevel;
+    public float currentLevelEXP;
+    public float nextLevelEXP;
+    public float remainingEXP;
+    private GameObject player;
+    public ParticleSystem LevelEffect;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        currentLevel = 1;
+        currentLevelEXP = 0;
+        remainingEXP = EXPCurve(2);
+        nextLevelEXP = EXPCurve(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentLevelEXP = player.GetComponent<CharacterStats>().exp - EXPCurve(currentLevel);
+        remainingEXP = nextLevelEXP - currentLevelEXP;
+        if(remainingEXP <= 0)
+        {
+            LevelUp();
+        }
+    }
+
+    void GainEXP(float exp)
+    {
+       player.GetComponent<CharacterStats>().exp += exp; 
+    }
+
+    float EXPCurve(int level)
+    {
+        return (level ^ 2 + level) / 2 * 100 - (level * 100);
+    }
+
+    void LevelUp()
+    {
+        LevelEffect.Play();
+        currentLevel += 1;
+        player.GetComponent<BaseClass>().GainStats();
+    }
+}
+
+    
