9a1c6f67c63265e2ca36deac2a73bf4a15894a4a
Minitte <arcentalminitte@gmail.com>
2018-10-14 23:04:06 -0700

    teleport menu

---
+++ Assets/Scripts/FloorTeleportMenuController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FloorTeleportMenuController : MonoBehaviour {
+
+	public Button[] floorButtons;
+
+	public int[] floorDestinations;
+
+	public PlayerManager playerManager;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start() {
+		playerManager = GameObject.Find("Player Manager").GetComponent<PlayerManager>();
+		updateButtons();
+	}
+
+	/// <summary>
+	/// Updates button visability based on the floor reached
+	/// </summary>
+	private void updateButtons() {
+		for (int i = 0; i < floorButtons.Length; i++) {
+			floorButtons[i].interactable = floorDestinations[i] <= playerManager.floorReached;
+		}
+	}
+
+	/// <summary>
+	/// Changes the scene to tower
+	/// </summary>
+	public void ChangeSceneToTower() {
+		SceneManager.LoadScene("Tower");
+	}
+	
+	/// <summary>
+	/// Changes the scene to town
+	/// </summary>
+	public void ChangeSceneToTown() {
+		SceneManager.LoadScene("Town");
+	}
+}
