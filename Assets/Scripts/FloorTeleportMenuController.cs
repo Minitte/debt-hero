378e445e193ffddbe9e15991d31930849442f9f2
Davis Pham <A00962164@ad.bcit.ca>
2018-10-15 21:09:40 -0700

    teleport menu controls

--- Assets/Scripts/FloorTeleportMenuController.cs
+++ Assets/Scripts/FloorTeleportMenuController.cs
@@ -4,11 +4,46 @@ using UnityEngine.SceneManagement;
 
 public class FloorTeleportMenuController : MonoBehaviour {
 
+	/// <summary>
+	/// Floor button list
+	/// </summary>
 	public Button[] floorButtons;
 
+	/// <summary>
+	/// Floor destination level list
+	/// </summary>
 	public int[] floorDestinations;
 
+	/// <summary>
+	/// Selection indicator
+	/// </summary>
+	public GameObject indicator;
+
+	/// <summary>
+	/// Delay for input
+	/// </summary>
+	[Range(0.05f, 0.5f)]
+	public float delay;
+
+	/// <summary>
+	/// Player manager reference
+	/// </summary>
 	private PlayerManager _playerManager;
+
+	/// <summary>
+	/// current menu index
+	/// </summary>
+	private int _currentIndex;
+
+	/// <summary>
+	/// menu is blocked from input
+	/// </summary>
+	private bool _menuBlocked;
+
+	/// <summary>
+	/// Time var for counting delay
+	/// </summary>
+	private float _time;
 
 	/// <summary>
 	/// Start is called on the frame when a script is enabled just before
@@ -17,25 +52,76 @@ public class FloorTeleportMenuController : MonoBehaviour {
 	void Start() {
 		_playerManager = PlayerManager.instance;
 		
-		updateButtons();
+		UpdateButtons();
 	}
 
 	/// <summary>
 	/// Update is called every frame, if the MonoBehaviour is enabled.
 	/// </summary>
-	void Update()
-	{
-		float cancel = Input.GetAxis("Cancel");
-
-		if (cancel != 0) {
-			this.gameObject.SetActive(false);
+	void Update(){
+		float vert = Input.GetAxis("Menu Nav Vertical");
+		
+		if (Mathf.Abs(vert) > 0.1) {
+			vert = Mathf.Sign(vert);
+		} else {
+			vert = 0f;
+			_menuBlocked = false;
+			_time = 0;
 		}
+
+		// menu cooldown
+		if (_menuBlocked) {
+			_time += Time.deltaTime;
+
+			if (_time > delay) {
+				_menuBlocked = false;
+				_time = 0;
+			}
+
+			return;
+		}
+
+		if (vert == 1) {
+			_menuBlocked = true;
+			NextButton();
+		} else if (vert == -1) {
+			_menuBlocked = true;
+			PrevButton();
+		}
+	}
+
+	/// <summary>
+	/// Shows the next panel
+	/// </summary>
+	public void NextButton() {
+		_currentIndex += 1 + floorButtons.Length;
+		_currentIndex = _currentIndex % floorButtons.Length;
+
+		SetIndicatorPosition(_currentIndex);
+	}
+
+	/// <summary>
+	/// Shows the previous panel
+	/// </summary>
+	public void PrevButton() {
+		_currentIndex += floorButtons.Length - 1;
+		_currentIndex = _currentIndex % floorButtons.Length;
+	
+		SetIndicatorPosition(_currentIndex);
+	}
+
+	/// <summary>
+	/// Sets the indicator position
+	/// </summary>
+	/// <param name="i"></param>
+	private void SetIndicatorPosition(int i) {
+		indicator.transform.SetParent(floorButtons[i].transform, false);
 	}
 
 	/// <summary>
 	/// Updates button visability based on the floor reached
 	/// </summary>
-	private void updateButtons() {
+	private void UpdateButtons() {
 		for (int i = 0; i < floorButtons.Length; i++) {
 			floorButtons[i].interactable = floorDestinations[i] <= GameState.floorReached;
 		}
