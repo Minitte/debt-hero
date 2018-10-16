f4bdf1b939d409990f5e4765de8f1c851624e474
Anthony Wong <awong6201@gmail.com>
2018-10-16 01:33:17 -0700

    Added a goblin enemy

--- Assets/Scripts/SkillCaster.cs
+++ Assets/Scripts/SkillCaster.cs
@@ -132,6 +132,8 @@ public class SkillCaster : MonoBehaviour {
                     case 0:
                         if (transform.GetComponent<Animator>()) {
                             transform.GetComponent<Animator>().SetTrigger("Attack");
+                        } else if (transform.GetChild(0).GetComponent<Animation>()) {
+                            transform.GetChild(0).GetComponent<Animation>().Play("attack");
                         }
                         Instantiate(skill0, transform); // Create the basic attack hitbox
                         break;
