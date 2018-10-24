8c4b598c789402a88d8783dcf2215ed73e321459
Minitte <arcentalminitte@gmail.com>
2018-10-23 21:32:47 -0700

    base item scripts

---
+++ Assets/Scripts/Items/BaseItem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class BaseItem : MonoBehaviour {
+
+    #region Events and Delegates
+
+    /// <summary>
+    /// delegate of a simple itemevent
+    /// </summary>
+    public delegate void SimpleItemEvent();
+
+    /// <summary>
+    /// delegate that requires a refernece of the current item 
+    /// </summary>
+    /// <param name="item">Current item</param>
+    public delegate void ReferenceItemEvent(BaseItem item);
+
+    /// <summary>
+    /// Event triggered when this item is used
+    /// </summary>
+    public event SimpleItemEvent OnUse;
+
+    /// <summary>
+    /// Event triggered when this item is about to be destoryed by disposal or zero qualitity
+    /// </summary>
+    public event ReferenceItemEvent OnDisposal;
+
+    #endregion
+
+    /// <summary>
+    /// properties of the item
+    /// </summary>
+	public ItemProperties properties;
+
+    /// <summary>
+    /// Owner of the item
+    /// </summary>
+    public BaseCharacter owner;
+
+    /// <summary>
+    /// Uses the item aslong the quantity is above 0 and is usable
+    /// </summary>
+    public void Use() {
+        // check qty
+        if (properties.quantity <= 0 || properties.usable) {
+            return;
+        }
+
+        properties.quantity--;
+
+        if (OnUse != null) {
+            OnUse();
+        }
+
+        // destory item if qualtity is <= 0
+        if (properties.destoryOnNone && properties.quantity <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// This function is called when the MonoBehaviour will be destroyed.
+    /// </summary>
+    void OnDestroy() {
+
+        // trigger event for listener
+        if (OnDisposal != null) {
+            OnDisposal(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Items/ItemProperties.cs
@@ -0,0 +1,52 @@
+using System;
+
+[Serializable]
+public class ItemProperties {
+	
+    public enum Type {
+        MISC,
+        CONSUMABLE,
+        EQUIPMENT,
+        QUEST
+    }
+
+    /// <summary>
+    /// Item name
+    /// </summary>
+    public string name = "ITEM_NAME";
+
+    /// <summary>
+    /// Type of item
+    /// </summary>
+    public Type type;
+
+    /// <summary>
+    /// boolean if item is stackable
+    /// </summary>
+    public bool stackable = true;
+
+    /// <summary>
+    /// Destory item when quantity = 0
+    /// </summary>
+    public bool destoryOnNone = true;
+
+    /// <summary>
+    /// if the item can be discard
+    /// </summary>
+    public bool disposable = true;
+
+    /// <summary>
+    /// if the item can be used
+    /// </summary>
+    public bool usable;
+
+    /// <summary>
+    /// item's gold value
+    /// </summary>
+    public int value;
+
+    /// <summary>
+    /// Item quantity
+    /// </summary>
+    public int quantity = 1;
+}
