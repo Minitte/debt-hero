8cb1aeaa7e20b906a1fb33a8b914f8df2b69ef59
Anthony Wong <awong6201@gmail.com>
2018-10-24 00:47:15 -0700

    Removed commented code

--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -6,6 +6,9 @@
 [CreateAssetMenu(menuName = "New Skill")]
 public class Skill : ScriptableObject {
     
+    /// <summary>
+    /// Enum for the different types of skills.
+    /// </summary>
     public enum SkillType {
         Melee,
         Ranged,
@@ -87,96 +90,4 @@ public class Skill : ScriptableObject {
             behaviourObj.Activate(caster, this);
         }
     }
-
-    /*
-    public Skill()
-    {
-        name = "";
-        cooldown = 0;
-        stat = CharacterStats.StatType.NONE;
-        amount = 0;
-        duration = 0;
-        type = -1;
-    }
-    
-    
-
-    /// <summary>
-    /// ALL SKILL INFO IS HERE
-    /// Gets info for a skill by Skill ID
-    /// Skill.type is -1 if skill not found
-    /// </summary>
-    public static Skill GetInfo(int skillID)
-    {
-        Skill skill = new Skill();
-
-        switch (skillID)
-        {
-            case 0:
-                skill.name = "Basic Attack";
-                skill.amount = 5;
-                skill.type = 0;
-                skill.cooldown = 1;
-                break;
-            case 1:
-                skill.name = "Healing";
-                skill.stat = CharacterStats.StatType.CURRENT_HP;
-                skill.amount = 20;
-                skill.type = 1;
-                skill.cooldown = 30;
-                break;
-        }
-
-        return skill;
-    }
-
-    /// <summary>
-    /// Skill Type 1
-    /// To use a buff skill that increases a stat, for recovery
-    /// (i.e.) Gain("hp", 10); gain 10 hp
-    /// Should only be used for HP and MP
-    /// </summary>
-    public static void Gain(CharacterStats target, CharacterStats.StatType stat, float amount)
-    {
-        //target.GetComponent<Animator>().SetBool("skillcast", true);
-        target.AddToStat(stat, amount);
-    }
-    /// <summary>
-    /// Skill Type 2
-    /// To use a buff skill that buffs a stat by amount for duration
-    /// (i.e.) BuffDuration("hp", 10, 10); buffs MaxHP by 10 for 10 seconds
-    /// </summary>
-    public static void BuffDuration(string stat, float duration, float amount)
-    {
-
-    }
-    /// <summary>
-    /// Skill Type 3
-    /// To use a buff skill that gives stat by amount/second for duration
-    /// (i.e.) BuffOverTime("hp", 10, 10); gives 10 HP per second for 10 seconds
-    /// </summary>
-    public static void BuffOverTime(string stat, float duration, float amount)
-    {
-
-    }
-
-    /// <summary>
-    /// Skill Type 4
-    /// To use a melee skill that does damage
-    /// </summary>
-    public static void Melee(float damage, Collider enemy)
-    {
-        
-        enemy.GetComponent<CharacterStats>().TakeDamage(damage, 0);
-    }
-
-    /// <summary>
-    /// Skill Type 5
-    /// To use a ranged skill
-    /// </summary>
-    public static void Ranged(float damage)
-    {
-
-    }
-    */
 }
--- Assets/Scripts/Skills/SkillCaster.cs
+++ Assets/Scripts/Skills/SkillCaster.cs
@@ -46,57 +46,16 @@ public class SkillCaster : MonoBehaviour {
         for (int i = 0; i < timestamps.Length; i++) {
             timestamps[i] = Time.time;
         }
-        /*
-        canCast0 = canCast1 = canCast2 = canCast3 = canCast4 = true;
-        timeStamp0 = timeStamp1 = timeStamp2 = timeStamp3 = timeStamp4 = Time.time;
-        test.character = transform;
-        */
     }
 
     // Update is called once per frame
     void Update() {
-        //Debug.Log("Cooldown Skill1: "+ (timeStamp1-Time.time));
-        //canCast0 = canCast1 = canCast2 = canCast3 = canCast4 = false;
+        // Check if cooldowns are over
         for (int i = 0; i < timestamps.Length; i++) {
             if (timestamps[i] <= Time.time) {
                 canCasts[i] = true;
             }
         }
-        /*
-        /// <summary>
-        /// If skill time is less than current time, skill0 can be casted again
-        /// </summary>
-        if (timeStamp0 <= Time.time) {
-            canCast0 = true;
-        }
-        /// <summary>
-        /// If skill time is less than current time, skill1 can be casted again
-        /// </summary>
-        if (timeStamp1 <= Time.time) {
-            canCast1 = true;
-        }
-
-        /// <summary>
-        /// If skill time is less than current time, skill2 can be casted again
-        /// </summary>
-        if (timeStamp2 <= Time.time) {
-            canCast2 = true;
-        }
-
-        /// <summary>
-        /// If skill time is less than current time, skill3 can be casted again
-        /// </summary>
-        if (timeStamp3 <= Time.time) {
-            canCast3 = true;
-        }
-
-        /// <summary>
-        /// If skill time is less than current time, skill4 can be casted again
-        /// </summary>
-        if (timeStamp4 <= Time.time) {
-            canCast4 = true;
-        }
-        */
     }
 
     /// <summary>
@@ -104,8 +63,8 @@ public class SkillCaster : MonoBehaviour {
     /// </summary>
     /// <param name="skillNum">The index of the skill to cast</param>
     public void Cast(int skillNum) {
-        if (skillNum < skills.Length && skills[skillNum] != null && canCasts[skillNum] == true) {
-            if (_characterStats.currentMp >= skills[skillNum].manaCost) {
+        if (skillNum < skills.Length && skills[skillNum] != null && canCasts[skillNum] == true) { // Validation
+            if (_characterStats.currentMp >= skills[skillNum].manaCost) { // Check if enough mana to cast
                 skills[skillNum].Cast(transform); // Cast the skill
                 _characterStats.currentMp -= skills[skillNum].manaCost;
 
@@ -114,100 +73,9 @@ public class SkillCaster : MonoBehaviour {
                 canCasts[skillNum] = false;
 
                 if (OnSkillCasted != null) {
-                    OnSkillCasted();
+                    OnSkillCasted(); // Fire the skill casted event
                 }
             }
         }
-
-        /*
-        bool cast = false;
-        
-        /*
-        switch (skillNum) {
-            case 0:
-                if (canCast0) {
-                    cast = true;
-                }
-                break;
-            case 1:
-                if (canCast1) {
-                    cast = true;
-                }
-                break;
-            case 2:
-                if (canCast2) {
-                    cast = true;
-                }
-                break;
-            case 3:
-                if (canCast3) {
-                    cast = true;
-                }
-                break;
-            case 4:
-                if (canCast4) {
-                    cast = true;
-                }
-                break;
-        }
-        
-        if (cast) {
-            /*
-            Skill currentSkill = Skill.GetInfo(skillID);
-            if (currentSkill.type != -1) {
-                switch (Skill.GetInfo(skillID).type) {
-                    case 0:
-                        if (GetComponent<BaseCharacter>().animator != null) {
-                            GetComponent<BaseCharacter>().animator.SetTrigger("Attack");
-                        }
-                        Instantiate(skill0, transform); // Create the basic attack hitbox
-                        break;
-                    case 1:
-                        //Skill.Gain(GetComponent<CharacterStats>(), currentSkill.stat, currentSkill.amount);
-                        Instantiate(skill1, transform);
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                }
-                
-                UpdateCooldown(skillNum, currentSkill.cooldown);
-            }
-            
-        }
-        */
     }
-    /*
-    /// <summary>
-    /// For updating the cooldowns
-    /// </summary>
-    private void UpdateCooldown(int skillNum, float cooldown) {
-        
-        switch (skillNum) {
-            case 0:
-                timeStamp0 += cooldown;
-                break;
-            case 1:
-                timeStamp1 += cooldown;
-                break;
-            case 2:
-                timeStamp2 += cooldown;
-                break;
-            case 3:
-                timeStamp3 += cooldown;
-                break;
-            case 4:
-                timeStamp4 += cooldown;
-                break;
-        }
-    }
-    */
-
-
-
 }
