19cb03844afa3fe26238156aa1c390e871683dfc
Ricky Mok <A00842318@ad.bcit.ca>
2018-10-30 16:18:00 -0700

    commented on time manager

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour {
+
+    // The directional light that represents the sun
     public Light sun;
-    public float timeScale = 2f;
-    public float secondsPerDay = 1800f;
-    public int days = 1;
+
+    /// <summary>
+    /// Time scale compared to real life
+    /// (i.e. 60f means 60x faster than real life
+    /// so 1 hour in game is 1 minute in real life
+    /// </summary>
+    public float timeScale = 60f;
+
+    // Seconds in a day, currently set to real life seconds
+    public float secondsPerDay = 86400f;
+
+
+    // Sun intensity and multiplier
     public float sunInitialIntensity;
     public float sunIntensityMultiplier;
+
+    // Text fields to update on time change
     public Text timeCounter;
     public Text dayNumber;
 
+    // Current time
     public float currentTime;
-
     public int currentHour;
     public int currentMinute;
+
+    // Number of days passed since game start. Starts at 1
+    public int days = 1;
+
+
     // Use this for initialization
     void Start() {
         currentTime = 0;
@@ -30,6 +49,8 @@ public class TimeManager : MonoBehaviour {
         currentHour = Mathf.RoundToInt(24 * currentTime);
         currentMinute = Mathf.RoundToInt(60 * (24 * currentTime - Mathf.Floor(24 * currentTime)));
         updateTime();
+
+        // When the current time is greater than 1 (24*1), one day has passed, so reset time to 0 and increment days
         if (currentTime >= 1) {
             days += 1;
             currentTime = 0;
@@ -37,6 +58,7 @@ public class TimeManager : MonoBehaviour {
         }
     }
 
+    // Updates sun position according to time
     void UpdateSun() {
         sun.transform.localRotation = Quaternion.Euler((currentTime * 360f) - 90, 170, 0);
 
@@ -53,6 +75,7 @@ public class TimeManager : MonoBehaviour {
         sun.intensity = sunInitialIntensity * sunIntensityMultiplier;
     }
 
+    // Updates the text fields to display time
     void updateTime() {
         if (currentHour >= 13) {
             if (currentHour - 12 < 10) {
