4ca9d60d085578c89eec17038d8b10ec8a8d4252
Jackee Ma <jackee.huy.ma@gmail.com>
2018-10-30 17:15:17 -0700

    Basic Dialog

---
+++ Assets/Scripts/UI/Dialog/Dialog.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour {
+    public List<string> dialogLines;
+
+    public DialogManager dialogManager;
+
+    public int dialogIndex;
+
+    public bool inRange;
+      
+    private void Update() {
+        if (inRange) {
+            if (Input.GetKeyDown(KeyCode.X)) {
+
+                if (dialogIndex >= dialogLines.Count) {
+                    dialogManager.CloseDialog();
+                    dialogIndex = 0;
+                    return;
+                }
+
+                dialogManager.ReadDialog(dialogLines[dialogIndex]);
+
+                dialogIndex++;
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+        dialogManager.CloseDialog();
+        inRange = false;
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        inRange = true;
+    }
+}
---
+++ Assets/Scripts/UI/Dialog/DialogManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogManager : MonoBehaviour {
+    /// <summary>
+    /// Background image of the dialog prefab.
+    /// </summary>
+    public Image dialogBackground;
+
+    /// <summary>
+    /// Text component of the dialog prefab.
+    /// </summary>
+    public Text dialogText;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ReadDialog(string line) {
+        ToggleDialog(true);
+        dialogText.text = line;
+    }
+
+    public void CloseDialog() {
+        ToggleDialog(false);
+    }
+
+    public void ToggleDialog(bool flag) {
+        dialogBackground.enabled = flag;
+        dialogText.text = "";
+        dialogText.enabled = flag;
+    }
+}
