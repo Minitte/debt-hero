f4f6bbb21948b1360b52ee5bc8d4be7348327351
Anthony Wong <awong6201@gmail.com>
2018-11-02 19:06:02 -0700

    Created a skill manager class and prefab

---
+++ Assets/Scripts/Skills/SkillManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This is a class for skill management.
+/// </summary>
+public class SkillManager : MonoBehaviour {
+
+    /// <summary>
+    /// Singleton instance of this class.
+    /// </summary>
+    public static SkillManager instance;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance != null) {
+            instance = this;
+        } else {
+            Debug.LogWarning("Tried to instantiate skill manager instance twice.");
+        }
+	}
+}
