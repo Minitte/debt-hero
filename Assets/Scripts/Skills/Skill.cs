c8739e14a1d59730c87e6d5a76bdcbf783091166
Anthony Wong <awong6201@gmail.com>
2018-11-13 16:29:52 -0800

    Fixed build error for PS4

--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 /// <summary>
 /// This is a scriptable class for skills.
 /// </summary>
@@ -99,9 +98,17 @@ public class Skill : ScriptableObject {
     /// </summary>
     private void Awake() {
         skillBehaviours = new List<SkillBehaviour>();
-        
-        // Reload subassets
+        /*
         Object[] assets = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(this));
+        foreach (Object o in assets) {
+            if (o is SkillBehaviour) {
+                skillBehaviours.Add(o as SkillBehaviour);
+            }
+        }
+        */
+
+        // Reload subassets
+        Object[] assets = Resources.LoadAll("Skills/" + name);
         foreach (Object o in assets) {
             if (o is SkillBehaviour) {
                 skillBehaviours.Add(o as SkillBehaviour);
