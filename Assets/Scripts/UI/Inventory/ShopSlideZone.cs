4f77057a3f847b0bfae98755293d97d5afd27afd
Minitte <arcentalminitte@gmail.com>
2018-11-14 22:15:49 -0800

    Added mouse exit enablers

--- Assets/Scripts/UI/Inventory/ShopSlideZone.cs
+++ Assets/Scripts/UI/Inventory/ShopSlideZone.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ShopSlideZone : MonoBehaviour, IPointerEnterHandler {
+public class ShopSlideZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
 	public InventoryPanel itemPanel;
 
@@ -17,7 +17,16 @@ public class ShopSlideZone : MonoBehaviour, IPointerEnterHandler {
 	/// <param name="eventData"></param>
 	public void OnPointerEnter(PointerEventData eventData) {
 		animator.SetBool("Seller", sellerSide);
+		itemPanel.enabled = true;
 	}
+
+	/// <summary>
+	/// Pointer enter Exit
+	/// </summary>
+	/// <param name="eventData"></param>
+	public void OnPointerExit(PointerEventData eventData) {
+		itemPanel.enabled = false;
+	} 
 
 	/// <summary>
 	/// Update is called every frame, if the MonoBehaviour is enabled.
@@ -28,12 +37,14 @@ public class ShopSlideZone : MonoBehaviour, IPointerEnterHandler {
 		if (sellerSide) {
 			if (lrswitch > 0) {
 				animator.SetBool("Seller", sellerSide);
+				itemPanel.enabled = true;
 			} else if (lrswitch < 0) {
 				itemPanel.enabled = false;
 			}
 		} else {
 			if (lrswitch < 0) {
 				animator.SetBool("Seller", sellerSide);
+				itemPanel.enabled = true;
 			} else if (lrswitch > 0) {
 				itemPanel.enabled = false;
 			}
