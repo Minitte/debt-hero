db0155852357a48f0cebf7898ff9dd55f589dcde
Minitte <arcentalminitte@gmail.com>
2018-11-15 00:02:49 -0800

    Single shop panel

---
+++ Assets/Scripts/UI/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour {
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update() {
+		float closeShop = Input.GetAxis("System Menu Open");
+		
+		if (closeShop != 0) {
+			this.gameObject.SetActive(false);
+		}
+	}
+}
