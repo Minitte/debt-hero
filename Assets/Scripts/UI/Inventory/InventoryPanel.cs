baeace4a9beff9958d98f84457656ec0666f69d5
Minitte <arcentalminitte@gmail.com>
2018-11-15 22:22:19 -0800

    fixed pricing on buy menu

--- Assets/Scripts/UI/Inventory/InventoryPanel.cs
+++ Assets/Scripts/UI/Inventory/InventoryPanel.cs
@@ -82,7 +82,7 @@ public class InventoryPanel : MonoBehaviour {
 	/// <summary>
 	/// current hovered or selected slot 
 	/// </summary>
-	private ItemSlot _currentSelectSlot;
+	protected ItemSlot _currentSelectSlot;
 
 	/// <summary>
 	/// Time for cool down
@@ -278,7 +278,7 @@ public class InventoryPanel : MonoBehaviour {
 	/// Shows the item details in the slot if any
 	/// </summary>
 	/// <param name="slot"></param>
-	protected void ShowItemDetails(ItemSlot slot) {
+	protected virtual void ShowItemDetails(ItemSlot slot) {
 		ItemBase item = _inventory.GetItem(slot);
 
 		GetGridSlot(_currentSelectSlot).SetBorderVisiblity(false);
@@ -420,7 +420,7 @@ public class InventoryPanel : MonoBehaviour {
 		}
 	}
 
-	private ItemGridItemUI GetGridSlot(ItemSlot slot) {
+	protected ItemGridItemUI GetGridSlot(ItemSlot slot) {
 		return itemRows[slot.row].items[slot.col];
 	}
 
