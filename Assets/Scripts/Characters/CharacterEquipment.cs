4f7709ae6772f1c6dd12d063e0f9fbfbe2fe8e44
Minitte <arcentalminitte@gmail.com>
2018-11-16 20:19:36 -0800

    equipping the equipment

---
+++ Assets/Scripts/Characters/CharacterEquipment.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class CharacterEquipment : MonoBehaviour {
+
+	public ItemEquipment weapon;
+
+	/// <summary>
+	/// Stats of the character
+	/// </summary>
+	private CharacterStats _stats;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start() {
+		_stats = GetComponent<CharacterStats>();
+	}
+
+	/// <summary>
+	/// Attempts to automaticly equip the item
+	/// </summary>
+	/// <param name="equipment"></param>
+	public void Equip(ItemEquipment equipment) {
+		switch (equipment.properties.type) {
+			case ItemProperties.Type.EQUIPMENT_WEAPON:
+				EquipWeapon(equipment);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	/// <summary>
+	/// Equips the give weapon and replaces the old one if any  
+	/// </summary>
+	/// <param name="wpn"></param>
+	public void EquipWeapon(ItemEquipment wpn) {
+		Debug.Assert(wpn.properties.type != ItemProperties.Type.EQUIPMENT_WEAPON, wpn.properties.name + " is not a weapon?");
+
+		weapon.RemoveBonusStats();
+
+		weapon = wpn;
+
+		wpn.AddBonusStats(); 
+	}
+}
--- Assets/Scripts/Items/ItemEquipment.cs
+++ Assets/Scripts/Items/ItemEquipment.cs
@@ -32,6 +32,27 @@ public class ItemEquipment : ItemBase {
     public int magDefence;
 
     /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start() {
+        OnUse += Equip;
+    }
+
+    /// <summary>
+    /// Equip the equipment if able to
+    /// </summary>
+    public void Equip() {
+        CharacterEquipment ce = GetComponent<CharacterEquipment>();
+
+        if (ce == null) {
+            return;
+        }
+
+        ce.Equip(this);
+    }
+
+    /// <summary>
     /// Adds bonus stats to the owner
     /// </summary>
     public void AddBonusStats() {
