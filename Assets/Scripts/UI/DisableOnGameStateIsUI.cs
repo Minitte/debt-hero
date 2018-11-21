a612d4fc09abeaa8865f191547d9eacb6a37ea3a
Davis Pham <arcentalminitte@gmail.com>
2018-11-20 17:34:55 -0800

    added a seperate script for hiding menu

---
+++ Assets/Scripts/UI/DisableOnGameStateIsUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnGameStateIsUI : MonoBehaviour {
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start() {
+		GameState.OnStateChanged += HandleGameStateChange;
+	}
+
+	/// <summary>
+	/// Handles game state changed event
+	/// </summary>
+	private void HandleGameStateChange() {
+		if (GameState.InAnyUIState()) {
+			this.gameObject.SetActive(false);
+		} else {
+			this.gameObject.SetActive(true);
+		}
+	}
+}
