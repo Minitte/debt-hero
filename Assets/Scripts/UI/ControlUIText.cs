e4502717c38e6214da8d93b44f3f135cc4157d83
jackee ma <jackee.huy.ma@gmail.com>
2018-12-01 15:22:17 -0800

    Added PS4 controls kinda. Still gotta test on Monday. Made Control unstructions simpler.

---
+++ Assets/Scripts/UI/ControlUIText.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlUIText : MonoBehaviour {
+    public List<string> controls;
+    public Text controlUI;
+    public int index;
+   
+
+    // Use this for initialization
+    void Start() {
+        SetControls();
+        index = 0;
+        controlUI.text = controls[0];
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetControls() {
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor) {
+             controls[0] = "Menu Controls \n"
+                 + "Open System Menu: Press ESC Key \n"
+                 + "Open In-game Menu: Press TAB Key \n"
+                 + "Menu Navigation: Move Mouse Cursor or D/W Key \n"
+                 + "Menu Confirm: Left Click or Press Enter Key \n"
+                 + "Menu Cancel: Left Click or Press ESC Key \n "
+                 + "Inventory Use: Right Click \n"
+                 + "Page 1/2";
+
+            controls[1] = "Combat Controls \n"
+                + "Movement: Press of Hold down Left Click \n"
+                + "Dash: Press Q to dash towards mouse cursor \n"
+                + "Basic Attack: Press Left Click on the enemy \n"
+                + "First Skill: Press W to activate skill 1 \n"
+                + "Second Skill: Press E to activate skill 2 \n"
+                + "Thrid Skill: Press R to activate skill 3 \n"
+                + "Page 2/2";
+        }else if(Application.platform == RuntimePlatform.PS4) {
+            controls[0] = "Menu Controls \n"
+                + "Open System Menu: Press R2 \n"
+                + "Open In-game Menu: Press TouchPad \n"
+                + "Menu Navigation: Use Left Analog Stick \n"
+                + "Menu Confirm: Press Circle \n"
+                + "Menu Cancel: Press Cross \n"
+                + "Inventory use: Press Triangle \n"
+                + "Page 1/2";
+
+            controls[1] = "Combat Controls \n"
+                + "Movement Horizontal: Left Analog \n"
+                + "Move Camera: Right Analog \n"
+                + "Dash: L1 \n"
+                + "Basic Attack: Cross \n"
+                + "First Skill: Circle \n"
+                + "Second Skill: Triangle \n"
+                + "Third Skill: Square \n"
+                + "Page 2/2";
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ControlsPanelManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsPanelManager : MonoBehaviour {
+    public Button backButton;
+    public Button setPage;
+    public ControlUIText controlUI;
+    public int index;
+	// Use this for initialization
+	void Start () {
+        index = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PS4controls();
+	}
+
+    public void CloseControlPanel() {
+        this.gameObject.SetActive(false);
+    }
+
+    public void OpenControlPanel() {
+        this.gameObject.SetActive(true);
+    }
+
+    public void SetPage() {
+        if (controlUI.index == 0) {
+            controlUI.index = 1;
+        } else if (controlUI.index == 1) {
+            controlUI.index = 0;
+        }
+        controlUI.controlUI.text = controlUI.controls[controlUI.index];
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+
+    }
+
+    public void PS4controls() {
+        if (Application.platform == RuntimePlatform.PS4) {
+            float hori = Input.GetAxis("Menu Horizontal");
+
+            if(hori < 0) {
+                index = 0;
+            }else if(hori > 0) {
+                index = 1;
+            }else if(hori == 0) {
+                index = -1;
+            }
+
+            if (index == 0) {
+                backButton.Select();
+            }else if(index == 1) {
+                setPage.Select();
+            }
+
+            if (Input.GetAxis("Menu Confirm") != 0) {
+                if(index == 0) {
+                    CloseControlPanel();
+                }else if(index == 1) {
+                    SetPage();
+                }
+            }
+
+        }
+    }
+
+}
