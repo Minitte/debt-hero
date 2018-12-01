ea23fbe85b92d49b612c800b21df04a24e0c315a
Anthony Wong <awong6201@gmail.com>
2018-11-30 21:54:23 -0800

    Better dash animation

--- Assets/Scripts/Skills/SkillHitboxes/AreaOfEffect.cs
+++ Assets/Scripts/Skills/SkillHitboxes/AreaOfEffect.cs
@@ -29,7 +29,7 @@ public class AreaOfEffect : SkillHitbox {
         GetComponent<SphereCollider>().radius *= _skill.areaMultiplier;
         
         // Start the attack
-        caster.GetComponent<BaseCharacter>().animator.SetTrigger("TakeDamage"); // Play attack animation
+        caster.GetComponent<BaseCharacter>().animator.SetTrigger("Take Damage"); // Play attack animation
         _active = true;
     }
 }
