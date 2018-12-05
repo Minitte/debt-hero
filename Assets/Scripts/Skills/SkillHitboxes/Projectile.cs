65465e90bd1dd84a86963cd5d2479a8665ca7fba
Anthony Wong <awong6201@gmail.com>
2018-12-05 05:36:44 -0800

    Added a projectile hitbox

---
+++ Assets/Scripts/Skills/SkillHitboxes/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This is a skill behaviour that deals damage in an arc infront of the player.
+/// </summary>
+public class Projectile : SkillHitbox {
+
+    // Update is called once per frame
+    private void Update() {
+        if (_damageFX != null) {
+            _damageFX.Emit(1); // Emit a damage effect particle
+        }
+    }
+
+    /// <summary>
+    /// Starts the melee attack.
+    /// </summary>
+    public override void Activate(BaseCharacter caster, Skill skill) {
+        // Setup melee properties
+        _physAtkdamage = caster.GetComponent<BaseCharacter>().characterStats.physAtk * skill.physicalMultiplier;
+        _magicAtkdamage = caster.GetComponent<BaseCharacter>().characterStats.magicAtk * skill.magicMultiplier;
+        _caster = caster;
+        _skill = skill;
+
+        // Setup projectile size
+        transform.localScale = Vector3.Scale(transform.localScale, skill.hitboxScale);
+        transform.localPosition = new Vector3(0f, transform.localScale.y, transform.localScale.z * 1.5f);
+        
+        // Start the projectile attack
+        caster.GetComponent<BaseCharacter>().animator.SetTrigger("Attack"); // Play attack animation
+        GetComponent<Collider>().enabled = true;
+    }
+}
