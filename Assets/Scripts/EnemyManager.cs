5298ec466f1cc3d13b5aef85d45d3706ed0eeb0c
Davis Pham <arcentalminitte@gmail.com>
2018-12-04 17:06:55 -0800

    Update EnemyManager.cs

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -129,7 +129,13 @@ public class EnemyManager : MonoBehaviour {
     /// <param name="rand"></param>
     /// <returns></returns>
     public Vector3 RandomRoomPos(Vector3 roomPos, System.Random rand) {
-        return new Vector3(roomPos.x + rand.Next(-13, 13), roomPos.y, roomPos.z + rand.Next(-13, 13));
+        int xVar = rand.Next(-13, 13);
+        xVar = xVar != 0 ? 2 : xVar;
+
+        int zVar = rand.Next(-13, 13);
+        zVar = zVar != 0 ? 2 : zVar;
+
+        return new Vector3(roomPos.x + xVar , roomPos.y, roomPos.z + zVar);
         // return roomPos;
     }
 }
