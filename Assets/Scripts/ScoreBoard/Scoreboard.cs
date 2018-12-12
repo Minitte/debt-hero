3f2152ada5c547a710d1f6163ece38d37a8f383d
Davis Pham <arcentalminitte@gmail.com>
2018-12-11 19:17:58 -0800

    Update Scoreboard.cs

--- Assets/Scripts/ScoreBoard/Scoreboard.cs
+++ Assets/Scripts/ScoreBoard/Scoreboard.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Scoreboard {
 
+    /// <summary>
+    /// Player prefs key for score
+    /// </summary>
     private static readonly string SCORE_KEY = "scores";
 
     /// <summary>
     /// Registers the player's score
     /// </summary>
+    /// <param name="playerName"></param>
     /// <param name="score"></param>
-    public static void RegisterScore(int score) {
-        List<int> scores = GetScores();
+    public static void RegisterScore(string playerName, int score) {
+        List<ScoreEntry> scores = GetScores();
 
-        scores.Add(score);
+        ScoreEntry entry = new ScoreEntry();
+
+        entry.name = playerName;
+    
+        entry.score = score;
+
+        scores.Add(entry);
 
         scores.Sort();
 
@@ -23,12 +34,12 @@ public class Scoreboard {
     /// Gets a list of scores
     /// </summary>
     /// <returns></returns>
-    public static List<int> GetScores() {
+    public static List<ScoreEntry> GetScores() {
         string jsonStr = PlayerPrefs.GetString(SCORE_KEY);
         ScoreList scoreList = (jsonStr.Length > 0) ? JsonUtility.FromJson<ScoreList>(jsonStr) : null;
 
         if (scoreList == null) {
-            List<int> scores = new List<int>();
+            List<ScoreEntry> scores = new List<ScoreEntry>();
 
             SaveScore(scores);
 
@@ -42,7 +53,7 @@ public class Scoreboard {
     /// Saves the list of scores
     /// </summary>
     /// <param name="scores"></param>
-    private static void SaveScore(List<int> scores) {
+    private static void SaveScore(List<ScoreEntry> scores) {
         ScoreList scoreList = new ScoreList();
         scoreList.scores = scores;
 
@@ -52,6 +63,21 @@ public class Scoreboard {
     }
 }
 
+[Serializable]
 class ScoreList {
-    public List<int> scores;
+    public List<ScoreEntry> scores;
+}
+
+[Serializable]
+public class ScoreEntry {
+
+    /// <summary>
+    /// name of player
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Score of the player
+    /// </summary>
+    public int score;
 }
